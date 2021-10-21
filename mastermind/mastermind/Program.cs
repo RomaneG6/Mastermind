c73a2d5be16f9b731427485dfe4481a5b8598544
RomaneG6 <91669844+RomaneG6@users.noreply.github.com>
2021-10-21 14:31:02 +0200

    combinaison du mastermind grp2

---
+++ mastermind/mastermind/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string combinaison;
+            int proposition;
+            int[] combi;
+            ChoixCombi(out combi);
+            Afficher(combi);
+            Console.WriteLine("Définir les couleurs choisies");
+            combinaison = Console.ReadLine();
+        }
+
+
+        static void ChoixCombi(out int[] combi) {
+            combi = new int[4];
+            Random rnd = new Random();
+            for (int i = 0; i <= 3; i++) {
+                int valeur = rnd.Next(1, 7);
+                combi[i] = valeur;
+            }
+        }
+
+        static void Afficher(int[] combi) {
+            for (int i = 0; i <= 3; i++) {
+                Console.Write(combi[i]);
+            }
+        }
+
+        static void stringToArray(string combinaison, out int[] proposition)
+       {
+            proposition = new int[4];
+            for (int i = 0; i <= 3; i++)
+            {
+               proposition[i] = combinaison[i]
+            }
+
+        }
+
+    }
+}
